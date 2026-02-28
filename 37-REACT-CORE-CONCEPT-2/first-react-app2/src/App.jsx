5d83f2b5bb9af2cc15253283a81b463b7a2d2d32
armaneousuf <armaneousuf97@gmail.com>
2026-02-28 13:03:32 +0600

    feat: added buttons

--- 37-REACT-CORE-CONCEPT-2/first-react-app2/src/App.jsx
+++ 37-REACT-CORE-CONCEPT-2/first-react-app2/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 
 function App() {
+  function handleBTN1() {
+    alert("Clicked btn 1");
+  }
+
+  function handleBTN2(n) {
+    alert(`The total is: ${n+5}`);
+  }
 
   return (
-    <>
-     
+    <div>
+     <button onClick={handleBTN1}>Click me</button>
+     <button onClick={()=> handleBTN2(5)}>Click me</button>
        
-    </>
+    </div>
   )
 }
 
